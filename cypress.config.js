c57a201a8af52ea99b564bc4fdea45f12447cacc
Jéssica de Melo  Souza <83194226+jessicamelo2015@users.noreply.github.com>
2024-05-06 20:15:42 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
         
      
       // implement node event listeners here
-    },  baseUrl:"https://www.saucedemo.com"
+    },  baseUrl:"https://www.saucedemo.com/"
 
     
   },
